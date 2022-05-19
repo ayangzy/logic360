805a56f2c3f91593f406fb612411334b77101e49
Ayange Felix <ayangefelix8@gmail.com>
2022-05-19 12:00:21 +0100

    work on login logic

---
+++ database/seeders/DefaultAdminSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+
+class DefaultAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $password = 'logic360ate';
+        User::updateOrCreate([
+            'first_name' => 'Fater',
+            'last_name' => 'Atekombo',
+            'phone_number' => '08134949093',
+            'email' => 'logicfatee360@gmail.com',
+            'password' => Hash::make($password),
+            'email_verified_at' => Carbon::now(),
+            'role' => 'super_admin',
+        ]);
+    }
+}
