ef0fa08e82da3bcd57d3f09905cba853411c025c
Ayange Felix <ayangefelix8@gmail.com>
2022-09-12 16:47:16 +0100

    add chat widget

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -1,25 +1,43 @@
 <!DOCTYPE html>
 <html lang="en-US" itemscope="itemscope" itemtype="http://schema.org/WebPage">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
-        <title>Market price reviewer</title>
-        <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/bootstrap.min.css') }}" media="all" />
-        {{-- <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/font-awesome.min.css') }}" media="all"/> --}}
-        <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/bootstrap-grid.min.css') }}" />
-        <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/bootstrap-reboot.min.css') }}" media="all" />
-        <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/font-techmarket.css') }}" media="all" />
-        <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/slick.css') }}" media="all" />
-        <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/techmarket-font-awesome.css') }}" media="all" />
-        <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/slick-style.css') }}" media="all" />
-        <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/animate.min.css') }}" media="all" />
-        <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/style.css') }}" media="all" />
-        <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/colors/blue.css') }}" media="all" />
-        
-        <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,900" rel="stylesheet">
-        <link rel="shortcut icon" href="assets/images/fav-icon.png">
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <title>Market price reviewer</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/bootstrap.min.css') }}" media="all" />
+    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/font-awesome.min.css') }}" media="all"/> --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/bootstrap-grid.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/bootstrap-reboot.min.css') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/font-techmarket.css') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/slick.css') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/techmarket-font-awesome.css') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/slick-style.css') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/animate.min.css') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/style.css') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/colors/blue.css') }}" media="all" />
+
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,900" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/images/fav-icon.png">
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {}
+            , Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script")
+                , s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/631f29ac54f06e12d8942ebb/1gcos9653';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+
+    </script>
+    <!--End of Tawk.to Script-->
+</head>
 <body class="woocommerce-active full-width">
+
     <!-- ====================== Top Header ================================= -->
     @include('front.layouts.inc.top-header')
 
