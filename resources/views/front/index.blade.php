ddb5f1a958a1e088090b3f2a215b006dd2d85f1a
Ayange Felix <ayangefelix8@gmail.com>
2022-10-13 15:17:55 +0100

    ui changes

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,11 +1,12 @@
 @extends('front.layouts.app')
 
+
 @section('content')
 
 <div id="content" class="site-content" tabindex="-1">
     @include('front.layouts.inc.slider')
     <div class="col-full">
-       
+
         <div class="row">
 
             <div id="primary" class="content-area">
@@ -20,12 +21,12 @@
 
                         <h1 class="woocommerce-products-header__title page-title">Available Products</h1>
                     </div>
-                   
+
                     <div id="grid-extended" role="tabpanel">
                         <div class="woocommerce columns-7">
                             <div class="products">
                                 @foreach ($products as $product)
-                                <div class="product first card shadow rounded mb-2">
+                                <div class="product first card shadow rounded mb-2 m-1" style="width: 100%; margin: 0">
 
                                     <a class="woocommerce-LoopProduct-link woocommerce-loop-product__link" href="">
                                         <img alt="" class="attachment-shop_catalog size-shop_catalog wp-post-image card-img-top" src="{{$product->image_url}}">
